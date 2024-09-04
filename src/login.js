939bbc1667f3a0cc43bac5ed1a53d6ea2b80c4a2
Willipatas <wgmedina@gmail.com>
2024-09-04 07:40:36 -0500

    Se realiza arreglos en los formularios y el CSS para que todos tomen los mismos estilos

--- src/login.js
+++ src/login.js
@@ -1,44 +1,45 @@
 window.onload = (event) => {
-    const loginForm = document.getElementById('inicio_usuarios');
+    const loginForm = document.getElementById('loginForm');
     const loginMessage = document.getElementById('loginMessage');
+    const apiUrl = 'http://localhost:3000'; 
 
-    loginForm.addEventListener('submit', async function(event){
+    // Asegúrate de que loginForm existe antes de agregar el event listener
+    if (loginForm) {
+        loginForm.addEventListener('submit', async function(event){
+            event.preventDefault(); // Prevenir que se envie el formulario de manera tradicional.
 
-        event.preventDefault(); // Prevenir que se envie el formulario de manera tradicional.
+            const username = document.getElementById('empleado_id').value;
+            const password = document.getElementById('contrasena').value;
+            console.log(username, password);
+            try {
+                const response = await fetch(`${apiUrl}/api/login`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({"empleado_id": username, "contrasena": password}),
+                });
 
-        const username = document.getElementById('empleado_id').value;
-        const password = document.getElementById('contrasena').value;
+                const data = await response.json();
+                console.log(data);
+                const encodeData = btoa(JSON.stringify(data));
+                console.log(encodeData);
 
-        try {
-            const response = await fetch('http://localhost:3000/api/login', {
-                method: 'POST',
-                mode: 'cors',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                credentials: 'include',
-                body: JSON.stringify({"empleado_id": username, "contrasena": password}),
-            });
-
-            const data = await response.json();
-            console.log(data);
-            const encodeData = btoa(JSON.stringify(data));
-            console.log(encodeData);
-
-            if (response.ok) {
-                loginMessage.textContent = 'Login Exitoso';
-                loginMessage.style.color = 'green';
-    //            window.location.href = `/dashboard.html#${encodeData}`;
-            } else {
-                loginMessage.textContent = data.message || 'Error en el Login';
-                loginMessage.style.color = 'red'
+                if (response.ok) {
+                    loginMessage.textContent = 'Login Exitoso';
+                    loginMessage.style.color = 'green';
+                    window.location.href = `dashboard.html#${encodeData}`;
+                } else {
+                    loginMessage.textContent = data.message || 'Error en el Login';
+                    loginMessage.style.color = 'red';
+                }
+            } catch (error) {
+                console.log(error);
+                loginMessage.textContent = 'Hubo un Error en el Login';
+                loginMessage.style.color = 'red';
             }
-        } catch (error) {
-            console.log (error)
-            loginMessage.textContent = 'Hubo un Error en el Login';
-            loginMessage.style.color = 'red'
-        }
-
-    });
-
+        });
+    } else {
+        console.error('Formulario de login no encontrado.');
+    }
 };
