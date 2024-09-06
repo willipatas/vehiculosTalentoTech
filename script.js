16eb66867cdaee7909d90e3074b6f72ec9ee4b6d
Willipatas <wgmedina@gmail.com>
2024-09-06 17:18:42 -0500

    Se correrige la página de registro de clientes, se crea el js para que el registro de nuevos clientes funcione y se hacen otras correcciones en el script para que las contraseñas se validen y todo funciona correctamente

--- script.js
+++ script.js
@@ -116,17 +116,34 @@ window.addEventListener('load', () => {
 
     document.addEventListener("DOMContentLoaded", function() {
         const form = document.querySelector("form");
-        const password = document.getElementById("password");
-        const confirmPassword = document.getElementById("confirm-password");
+        const contrasena = document.getElementById("contrasena");
+        const confirmContrasena = document.getElementById("confirmContrasena");
         const errorMessage = document.getElementById("error-message");
 
         form.addEventListener("submit", function(event) {
             // Verificar si las contraseñas coinciden
-            if (password.value !== confirmPassword.value) {
+            if (contrasena.value !== confirmContrasena.value) {
                 event.preventDefault(); // Evita que el formulario se envíe
                 errorMessage.style.display = "block"; // Muestra el mensaje de error
             } else {
                 errorMessage.style.display = "none"; // Oculta el mensaje de error
             }
         });
+    });
+
+    // Script para el cierre de sesión
+    document.addEventListener('DOMContentLoaded', () => {
+        const logoutLink = document.getElementById('logoutLink');
+    
+        if (logoutLink) {
+            logoutLink.addEventListener('click', (event) => {
+                event.preventDefault(); // Previene el comportamiento por defecto del enlace
+    
+                // Elimina el token del almacenamiento local
+                localStorage.removeItem('token');
+    
+                // Redirige al usuario a la página de inicio de sesión
+                window.location.href = '/index.html'; 
+            });
+        }
     });
--- src/login.js
+++ src/login.js
@@ -1,6 +1,8 @@
 window.onload = (event) => {
     const loginForm = document.getElementById('loginForm');
     const loginMessage = document.getElementById('loginMessage');
+    const registrationForm = document.getElementById('registrationForm');
+    const registrationMessage = document.getElementById('registrationMessage');
     const apiUrl = 'http://localhost:3000'; 
 
     // Asegúrate de que loginForm existe antes de agregar el event listener
@@ -10,7 +12,7 @@ window.onload = (event) => {
 
             const username = document.getElementById('empleado_id').value;
             const password = document.getElementById('contrasena').value;
-            console.log(username, password);
+            
             try {
                 const response = await fetch(`${apiUrl}/api/login`, {
                     method: 'POST',
@@ -39,7 +41,46 @@ window.onload = (event) => {
                 loginMessage.style.color = 'red';
             }
         });
+
     } else {
         console.error('Formulario de login no encontrado.');
     }
+
+        registrationForm.addEventListener('submit', async function(event){
+            event.preventDefault(); // Prevenir que se envie el formulario de manera tradicional.
+
+            const nombre_cliente = document.getElementById('nombre_cliente').value;
+            const correo_cliente = document.getElementById('correo_cliente').value;
+            const telefono_cliente = document.getElementById('telefono_cliente').value;
+            const direccion_cliente = document.getElementById('direccion_cliente').value;
+            const ciudad_cliente = document.getElementById('ciudad_cliente').value;
+            const contrasena = document.getElementById('contrasena').value;
+            
+            try {
+                const response = await fetch(`${apiUrl}/nuevo_usuario`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({"nombre_cliente": nombre_cliente, "correo_cliente": correo_cliente, "telefono_cliente": telefono_cliente, "direccion_cliente": direccion_cliente, "ciudad_cliente": ciudad_cliente, "contrasena": contrasena}),
+                });
+
+                const data = await response.json();
+                console.log(data);
+                const encodeData = btoa(JSON.stringify(data));
+                console.log(encodeData);
+
+                if (response.ok) {
+                    registrationMessage.textContent = 'Registro Exitoso';
+                    registrationMessage.style.color = 'green';
+                } else {
+                    registrationMessage.textContent = data.message || 'Error en el registro';
+                    registrationMessage.style.color = 'red';
+                }
+            } catch (error) {
+                console.log(error);
+                registrationMessage.textContent = 'Hubo un Error en el registro';
+                registrationMessage.style.color = 'red';
+            }
+        });
 };
